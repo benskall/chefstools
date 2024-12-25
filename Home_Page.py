16bc1770e53ac787ad4a0aacfaa82dd437d004f5
Ben Skalka <benjaminskalka@gmail.com>
2024-12-25 01:10:37 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -1,16 +1,12 @@
 import streamlit as st
 
 # Title of App
-st.title("Web Development Lab03")
+st.title("Michelle's Chef Tools!")
 
 # Assignment Data 
 # TODO: Fill out your team number, section, and team members
 
-st.header("CS 1301")
-st.subheader("Team XX, Web Development - Section X")
-st.subheader("Team Member 1, Team Member 2")
-
-
+st.header("Each Page is a Different Tool For The Clean Plate Club")
 # Introduction
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
 #       1. **Page Name**: Description
@@ -19,7 +15,7 @@ st.subheader("Team Member 1, Team Member 2")
 #       4. **Page Name**: Description
 
 st.write("""
-Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+You can navigate between the pages using the sidebar to the left. The following pages are:
 
 1.
 2.
